61629cb6493fb345b243cc670c381a6adbe19fcd
Mathias FERNANDES <mathias.fernandes@etu.univ-grenoble-alpes.fr>
2024-06-14 10:59:28 +0200

    Commit classes restantes avec constructeur du modèle pour synchro

---
+++ src/main/java/fr/uga/iut2/genevent/modele/Acteur.java
@@ -0,0 +1,15 @@
+package fr.uga.iut2.genevent.modele;
+
+public class Acteur {
+    private String id;
+    private String nom;
+    private String prenom;
+    private String specialite;
+
+    public Acteur(String id, String nom, String prenom, String specialite) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.specialite = specialite;
+    }
+}
---
+++ src/main/java/fr/uga/iut2/genevent/modele/Animal.java
@@ -0,0 +1,11 @@
+package fr.uga.iut2.genevent.modele;
+
+public class Animal {
+    private String nom;
+    private String espece;
+
+    public Animal(String nom, String espece) {
+        this.nom = nom;
+        this.espece = espece;
+    }
+}
