59c946007e4a4421eec864ec6c00d5c75da548a4
Nicolas TSIGRIS <nicolas.tsigris@etu.univ-grenoble-alpes.fr>
2024-06-14 10:52:01 +0200

    CL Evenement/Organisateur 1

--- src/main/java/fr/uga/iut2/genevent/modele/Evenement.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Evenement.java
@@ -2,63 +2,69 @@ package fr.uga.iut2.genevent.modele;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Evenement implements Serializable {
 
-    private static final long serialVersionUID = 1L;  // nécessaire pour la sérialisation
-    private final GenEvent genevent;
-    private final String nom;
-    private LocalDate dateDebut;
-    private LocalDate dateFin;
-    private final Map<String, Utilisateur> administrateurs;  // association qualifiée par l'email
+    private long ID;
+    private String titre;
+    private String adresse;
+    private String dateDebut;
+    private String dateFin;
+    private ArrayList<Spectacle> spectacles;
+    private ArrayList<Organisateur> organisateurs;
 
-    // Invariant de classe : !dateDebut.isAfter(dateFin)
-    //     On utilise la négation ici pour exprimer (dateDebut <= dateFin), ce
-    //     qui est équivalent à !(dateDebut > dateFin).
-
-    public static Evenement initialiseEvenement(GenEvent genevent, String nom, LocalDate dateDebut, LocalDate dateFin, Utilisateur admin) {
-        Evenement evt = new Evenement(genevent, nom, dateDebut, dateFin);
-        evt.ajouteAdministrateur(admin);
-        return evt;
-    }
-
-    public Evenement(GenEvent genevent, String nom, LocalDate dateDebut, LocalDate dateFin) {
-        assert !dateDebut.isAfter(dateFin);
-        this.genevent = genevent;
-        this.nom = nom;
+    public Evenement(long ID, String titre, String adresse, String dateDebut, String dateFin) {
+        this.ID = ID;
+        this.titre = titre;
+        this.adresse = adresse;
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
-        this.administrateurs = new HashMap<>();
     }
 
-    public String getNom() {
-        return this.nom;
+    public Spectacle ajouterSpectacle(Spectacle spectacle){
+        spectacles.add(spectacle);
+        return spectacle;
     }
 
-    public LocalDate getDateDebut() {
+    public void setDateFin(String dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public void setDateDebut(String dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getDateDebut() {
         return dateDebut;
     }
 
-    public void setDateDebut(LocalDate dateDebut) {
-        assert !dateDebut.isAfter(this.dateFin);
-        this.dateDebut = dateDebut;
-    }
-
-    public LocalDate getDateFin() {
+    public String getDateFin() {
         return dateFin;
     }
 
-    public void setDateFin(LocalDate dateFin) {
-        assert !this.dateDebut.isAfter(dateFin);
-        this.dateFin = dateFin;
+    public Organisateur ajoutOganisateur(Organisateur organisateur){
+
     }
 
-    public void ajouteAdministrateur(Utilisateur admin) {
-        assert !this.administrateurs.containsKey(admin.getEmail());
-        this.administrateurs.put(admin.getEmail(), admin);
-        admin.ajouteEvenementAdministre(this);
+    public Spectacle getSppectacle(int i) {
+        return spectacles.get(i);
     }
+
+
 }
