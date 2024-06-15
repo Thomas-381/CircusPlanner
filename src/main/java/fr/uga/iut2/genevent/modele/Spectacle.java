447fd9e6e160e817153b28406b525254b560d52a
Johan Le Fol <johan.le-fol@etu.univ-grenoble-alpes.fr>
2024-06-15 19:36:38 +0200

    removed "nbPlaces" and "prix" from Spectacles as they belong to Evenements + added getters.

--- src/main/java/fr/uga/iut2/genevent/modele/Spectacle.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Spectacle.java
@@ -10,32 +10,22 @@ import java.util.HashSet;
 public class Spectacle {
     private String nom;
     private String lieu;
-    private int nbrPlaces;
-    private float prix;
-    private HashSet<Evenement> evenements = new HashSet<>();
     private ArrayList<Numero> numeros = new ArrayList<>();
     private HashSet<Organisateur> organisateurs = new HashSet<>();
 
     /**
-     * Construit un nouveau Spectacle avec le nom, le lieu, le nombre de places et le prix donnés.
+     * Construit un nouveau Spectacle avec le nom, le lieu.
      *
      * @param nom Le nom du Spectacle.
      * @param lieu Le lieu du Spectacle.
-     * @param nbrPlaces Le nombre de places du Spectacle.
-     * @param prix Le prix du Spectacle.
      */
-    public Spectacle(String nom, String lieu, int nbrPlaces, float prix) {
+    public Spectacle(String nom, String lieu) {
         this.nom = nom;
         this.lieu = lieu;
-        this.nbrPlaces = nbrPlaces;
-        this.prix = prix;
     }
-
-
 
     /**
      * Définit le nom du Spectacle.
-     *
      * @param nom Le nouveau nom du Spectacle.
      */
     public void setNom(String nom) {
@@ -44,26 +34,14 @@ public class Spectacle {
 
     /**
      * Définit le lieu du Spectacle.
-     *
      * @param lieu Le nouveau lieu du Spectacle.
      */
     public void setLieu(String lieu) {
         this.lieu = lieu;
     }
 
-
-    /**
-     * Définit le prix du Spectacle.
-     *
-     * @param prix Le nouveau prix du Spectacle.
-     */
-    public void setPrix(float prix) {
-        this.prix = prix;
-    }
-
     /**
      * Retourne le nom du Spectacle.
-     *
      * @return Le nom du Spectacle.
      */
     public String getNom() {
@@ -72,22 +50,41 @@ public class Spectacle {
 
     /**
      * Retourne le lieu du Spectacle.
-     *
      * @return Le lieu du Spectacle.
      */
     public String getLieu() {
         return this.lieu;
     }
 
-
-
     /**
      * Ajoute un organisateur au Spectacle.
-     *
      * @param organisateur L'organisateur à ajouter.
      */
     public void ajouterOrganisateur(Organisateur organisateur){
         organisateurs.add(organisateur);
     }
 
+    /**
+     * Ajoute un numéro au spectacle.
+     * @param numero Le numéro à ajouter.
+     */
+    public void ajouterNumero(Numero numero) {
+        numeros.add(numero);
+    }
+
+    /**
+     * Getter de l'attribut numeros.
+     * @return la liste ordonnée des numéros composants le spectacle.
+     */
+    public ArrayList<Numero> getNumeros() {
+        return numeros;
+    }
+
+    /**
+     * Getter de l'attribut organisateurs.
+     * @return la liste des organisateurs du spectacle.
+     */
+    public HashSet<Organisateur> getOrganisateurs() {
+        return organisateurs;
+    }
 }
