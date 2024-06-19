c997d02f245cbd18fb79e8b8908869fc1855bc73
Johan Le Fol <johan.le-fol@etu.univ-grenoble-alpes.fr>
2024-06-19 22:15:23 +0200

    ajout de logger pour la sauvegarde de l'application + commentaires liés à la sauvegarde

--- src/main/java/fr/uga/iut2/genevent/util/Sauvegarde.java
+++ src/main/java/fr/uga/iut2/genevent/util/Sauvegarde.java
@@ -6,6 +6,11 @@ import java.io.*;
 
 public class Sauvegarde {
 
+    /**
+     * Sauvegarde l'état de l'application dans un fichier
+     * @param application l'état de l'application actuel
+     * @throws IOException si le fichier n'est pas accessible
+     */
     public static void save(Application application) throws IOException{
         FileOutputStream fileOut = new FileOutputStream("sauvegarde/save.ser");
         ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -15,6 +20,12 @@ public class Sauvegarde {
 
     }
 
+    /**
+     * Lis le fichier de sauvegarde et restaure l'état de l'application
+     * @return l'état de l'application
+     * @throws IOException si le fichier de sauvegarde n'est pas trouvé
+     * @throws ClassNotFoundException si la classe Application n'existe pas
+     */
     public static Application load() throws IOException, ClassNotFoundException {
         Application app;
         FileInputStream fileIn = new FileInputStream("sauvegarde/save.ser");
--- src/main/java/fr/uga/iut2/genevent/vue/MainView.java
+++ src/main/java/fr/uga/iut2/genevent/vue/MainView.java
@@ -12,10 +12,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainView extends Application {
 
-    private static fr.uga.iut2.genevent.modele.Application app = new fr.uga.iut2.genevent.modele.Application();
+    private static final Logger LOGGER = Logger.getLogger(Sauvegarde.class.getPackageName());
+
+    private static fr.uga.iut2.genevent.modele.Application app;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -31,7 +35,12 @@ public class MainView extends Application {
     public static void main(String[] args){
         try {
             app = Sauvegarde.load();
+            LOGGER.log(Level.INFO, "données de l'application chargées");
         } catch (IOException e) {
+                    LOGGER.log(Level.WARNING, "Fichier de sauvegarde non trouvé");
+                    app = new fr.uga.iut2.genevent.modele.Application();
+
+                    // Peuplement de base de l'application pour tests (partie à supprimer à terme)
                     app.ajouterEvenement(new Evenement("event1", "quelque part", "53/43/8946", "17/28/7391", 700));
                     app.ajouterEvenement(new Evenement("event2", "autre part", "55/48/6846", "24/37/1468", 500));
                     app.ajouterSpectacle(new Spectacle("spectacle1", "quelque part"));
@@ -40,6 +49,8 @@ public class MainView extends Application {
                     app.ajouterNumero(new Numero("numero2"));
                     app.ajouterActeur(new Acteur("acteur1", "patrick", "Jean", "spécialiste"));
                     app.ajouterActeur(new Acteur("acteur2", "d'arc", "Jeanne", "très forte"));
+
+                    LOGGER.log(Level.INFO, "Création d'une base vide");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -49,7 +60,7 @@ public class MainView extends Application {
         try {
             Sauvegarde.save(app);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            LOGGER.log(Level.SEVERE, "fichier de sauvegarde non accessible, l'état n'a pas été sauvegardé");
         }
     }
 }
