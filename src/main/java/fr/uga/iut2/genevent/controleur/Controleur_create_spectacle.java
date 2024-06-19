f2f5f2ff8c403924664a85c7fe1708fc1e8a70a3
Aline ROSTAGNAT <aline.rostagnat@etu.univ-grenoble-alpes.fr>
2024-06-19 13:47:45 +0200

    ajout controlleur pour chaque page + Possibilité de changer image pour toutes les pages

---
+++ src/main/java/fr/uga/iut2/genevent/controleur/Controleur_create_spectacle.java
@@ -0,0 +1,44 @@
+package fr.uga.iut2.genevent.controleur;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controleur_create_spectacle {
+    @FXML
+    private ImageView imgSpectacleC;
+
+    @FXML
+    private void initialize() {
+        // Ajoutez ici toutes les ImageView pour lesquelles vous souhaitez appliquer la méthode de gestion des clics
+        ImageView[] imageViews = { imgSpectacleC/* Ajoutez ici d'autres ImageView */ };
+        setupImageViewClickHandler(imageViews);
+    }
+
+    private void setupImageViewClickHandler(ImageView... imageViews) {
+        for (ImageView imageView : imageViews) {
+            imageView.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                handleImageClick(imageView);
+            });
+        }
+    }
+
+    private void handleImageClick(ImageView imageView) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+        );
+
+        Stage stage = (Stage) imageView.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            Image image = new Image(selectedFile.toURI().toString());
+            imageView.setImage(image);
+        }
+    }
+}
